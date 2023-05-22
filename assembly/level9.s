b376838f4b04753b0a0d8cafd81a16020151cbfa
Dahir Muhammad Dahir <dahirmuhammad3@gmail.com>
2023-05-22 11:56:24 +0100

    update: completed level 9

---
+++ assembly/level9.s
@@ -0,0 +1,29 @@
+# -=-<[ Bismillahirrahmanirrahim ]>-=-
+# -*- coding: utf-8 -*-
+# @Date    : 2023-05-22 11:32:31
+# @Author  : Dahir Muhammad Dahir (dahirmuhammad3@gmail.com)
+# @Link    : link
+# @Version : 1.0.0
+
+
+
+# assemble with: gcc -nostdlib -o file file.s
+# dump with: objdump -M intel -d file
+# copy binary with: objcopy --dump-section .text=file_binary file
+
+.intel_syntax noprefix
+.global _start
+
+_start:
+    and rdi, 0x1; get the least bit in rdi, if it is 1, rdi is odd
+    and rax, 0x0; clear rax
+    xor rax, 0x1; set rax to 1
+    xor rax, rdi; if rdi is odd, rax is 0, else rax is 1
+
+
+finish:
+    mov rdi, 0x0;
+    mov rax, 0x3c;
+    syscall;
+
+
