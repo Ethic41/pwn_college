a1d58795d87ea7bd0b469fae4260c1dc57047481
Dahir Muhammad Dahir <dahirmuhammad3@gmail.com>
2023-05-30 18:10:37 +0100

    update: added webserver

--- assembly/webserver/level.s
+++ assembly/webserver/level.s
@@ -1,24 +1,25 @@
 # -=-<[ Bismillahirrahmanirrahim ]>-=-
 # -*- coding: utf-8 -*-
-# @Date    : 2023-05-19 17:01:46
+# @Date    : 2023-05-30 18:08:52
 # @Author  : Dahir Muhammad Dahir (dahirmuhammad3@gmail.com)
 # @Link    : link
 # @Version : 1.0.0
 
 
 
-# assemble with: gcc -nostdlib -o level2 level2.s
-# dump with: objdump -M intel -d level2
-# copy binary with: objcopy --dump-section .text=level2_binary level2
+# assemble with: gcc -nostdlib -o level1 level1.s
+# dump with: objdump -M intel -d level1
+# copy binary with: objcopy --dump-section .text=level1_binary level1
 
 .intel_syntax noprefix
 .global _start
 
 _start:
-    add rdi, 0x331337;
+    call finish;
 
 finish:
     mov rdi, 0x0;
     mov rax, 0x3c;
     syscall;
+
 
